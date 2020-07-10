d9a3d6bb4624a17ecf503e421eb5c6b88ee8fd61
HomeMadeProjects <53355080+HomeMadeProjects@users.noreply.github.com>
2020-07-10 14:50:32 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -21,3 +21,5 @@ rightBtn.addEventListener('click', function () {
     square.classList.add('right');
 
 });
+
+// stworzyć funkcję, która sprawdzi, jaka jest aktualnie dodana klasa (currentClass), jeśli klikam LEWO to dodaje 'left', jeśli jeszcze raz - blokuje przycisk (dodać klasę 'blocked'), jeśli klikam PRAWO to usuwa aktualną klasę i dodaje nową itp. Rotate musi działać cały czas, bez względu na położenie DIVa - JAK BY TO ZROBIĆ???
